594700a19dbad217428c392899b07f53f3158c4c
GV_Yich <yich.lin@Gravity.co.kr>
2017-06-20 11:14:51 +0800

    完成

--- DownloadCaptcha/Form1.Designer.cs
+++ DownloadCaptcha/Form1.Designer.cs
@@ -33,13 +33,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 96);
+            this.button1.Location = new System.Drawing.Point(150, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 45);
             this.button1.TabIndex = 0;
@@ -49,23 +51,33 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 96);
+            this.numericUpDown1.Location = new System.Drawing.Point(105, 96);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(39, 22);
             this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(13, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(180, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 50);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 125);
+            this.checkBox1.Location = new System.Drawing.Point(13, 97);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(72, 16);
             this.checkBox1.TabIndex = 3;
@@ -75,24 +87,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 98);
+            this.label1.Location = new System.Drawing.Point(150, 100);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 4;
             this.label1.Text = "執行次數";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "瀏覽";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(205, 153);
+            this.ClientSize = new System.Drawing.Size(238, 166);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "驗證碼下載器";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -107,6 +139,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DownloadCaptcha/Form1.cs
+++ DownloadCaptcha/Form1.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,6 +26,94 @@ namespace DownloadCaptcha
         {
             //執行次數
             int runTimes = Convert.ToInt32(numericUpDown1.Value);
+            //是否下載
+            bool isDownload = checkBox1.Checked;
+            if (isDownload) {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                    MessageBox.Show("請選擇檔案下載路徑");
+            }
+            //高鐵網站的網址
+            string thsrcUrl = "https://irs.thsrc.com.tw/";
+
+            //先進去高鐵網站取得cookie
+            var client = new RestClient(thsrcUrl);
+            client.CookieContainer = new System.Net.CookieContainer();
+            var request = new RestRequest("IMINT", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            //依據執行次數下載檔案
+            for (var i = 0; i <= runTimes; i++)
+            {
+                //取得驗證碼網址(會回傳圖片網址和聲音檔網址的xml)
+                client.BaseUrl = new Uri("https://irs.thsrc.com.tw/IMINT/?wicket:interface=:0:BookingS1Form:homeCaptcha:reCodeLink::IBehaviorListener&wicket:behaviorId=0&random=0.5316124304206924");
+                IRestResponse response2 = client.Execute(request);
+                var content2 = response2.Content;
+
+                Regex regex = new Regex(@"\/IMINT\/.*homeCaptcha:passCode[^\""]*");
+                Match m = regex.Match(content2);
+                //如果有match到驗證碼圖片地址
+                if (m.Success)
+                {
+                    var captchaUrl = m.Groups[0].Value;
+                    client.BaseUrl = new Uri(thsrcUrl);//再把baseUrl切換回來
+                    var fileBytes = client.DownloadData(new RestRequest(captchaUrl, Method.GET));
+                    if (isDownload)
+                    {
+                        File.WriteAllBytes(Path.Combine(textBox1.Text, string.Format(@"{0}.png", Guid.NewGuid())), fileBytes);
+                    }
+                    pictureBox1.Image = BufferToImage(fileBytes);
+                    pictureBox1.Refresh();
+                }
+                else {
+                    MessageBox.Show("無法匹配到驗證碼連結位址");
+                }
+            }
+        }
+
+        //將byte[]轉為Image
+        public static Image BufferToImage(byte[] Buffer)
+        {
+            if (Buffer == null || Buffer.Length == 0) { return null; }
+            byte[] data = null;
+            Image oImage = null;
+            Bitmap oBitmap = null;
+            //建立副本
+            data = (byte[])Buffer.Clone();
+            try
+            {
+                MemoryStream oMemoryStream = new MemoryStream(Buffer);
+                //設定資料流位置
+                oMemoryStream.Position = 0;
+                oImage = System.Drawing.Image.FromStream(oMemoryStream);
+                //建立副本
+                oBitmap = new Bitmap(oImage);
+            }
+            catch
+            {
+                throw;
+            }
+            //return oImage;
+            return oBitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string folderpath = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            fbd.ShowNewFolderButton = false;
+            fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            DialogResult dr = fbd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                folderpath = fbd.SelectedPath;
+            }
+
+            if (folderpath != "")
+            {
+                textBox1.Text = folderpath;
+            }
         }
     }
 }
